40d35b7e14042f32fde6c33055ed57f6137b0d7e
estiven2011 <juan_posada23221@elpoli.edu.co>
2025-09-08 17:41:11 -0500

    se crean carpetas y se configura el tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  darkMode: 'class',                 // ← usaremos clase para activar modo oscuro
+  content: ['./src/**/*.{html,ts}'], // ← escanea plantillas Angular
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        surface: {
+          900: '#0f1115', // fondo principal
+          800: '#14161b', // paneles
+          700: '#1b1e25', // hover
+        },
+        textc: {
+          base: '#e5e7eb', // texto claro
+          mute: '#9ca3af', // texto apagado
+        },
+        brand: {
+          500: '#84cc16', // verde acento
+          600: '#65a30d',
+        }
+      }
+    },
   },
   plugins: [],
 }
-
